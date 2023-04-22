fa3b1f89a533bbfe07c8323b7e84298a95ede890
Yuval Dolev <yuvaldolev2@gmail.com>
2023-04-23 01:41:47 +0300

    feat: implement the NN layer output calculation

--- Assets/Scripts/Neural Network/NewClass.cs
+++ Assets/Scripts/Neural Network/NewClass.cs
@@ -1,17 +1,36 @@
 ﻿public class Layer
 {
-	private int inNodesNumber;
-	private int outNodesNumber;
+	private int inputNodesNumber;
+	private int outputNodesNumber;
 	private double[,] weights;
 	private double[] biases;
 
-	public Layer(int inNodesNumber, int outNodesNumber)
+	public Layer(int inputNodesNumber, int outputNodesNumber)
 	{
-		this.inNodesNumber = inNodesNumber;
-		this.outNodesNumber = outNodesNumber;
+		this.inputNodesNumber = inputNodesNumber;
+		this.outputNodesNumber = outputNodesNumber;
 
-		weights = new double[this.inNodesNumber, this.outNodesNumber];
-		biases = new double[this.outNodesNumber];
+		weights = new double[this.inputNodesNumber, this.outputNodesNumber];
+		biases = new double[this.outputNodesNumber];
+	}
+
+	public double[] CalculateOutputs(double[] inputs)
+	{
+		var weightedInputs = new double[outputNodesNumber];
+
+		for (int outputNode = 0; outputNode < outputNodesNumber; ++outputNode)
+		{
+			double weightedInput = biases[outputNode];
+
+			for (int inputNode = 0; inputNode < inputNodesNumber; inputNode++)
+			{
+				weightedInput += inputs[inputNode] * weights[inputNode, outputNode];
+			}
+
+			weightedInputs[outputNode] = weightedInput;
+		}
+
+		return weightedInputs;
 	}
 }
 
